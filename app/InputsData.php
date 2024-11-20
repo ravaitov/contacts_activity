79eea64eca196f48f1a8f059e288ff8ecb0a2d80
ravil_aitov <ravaitov@gmail.com>
2024-11-20 09:05:25 +0300

    логика изменена (если есть о в итоге недели, то и ощий итог 0)

--- app/InputsData.php
+++ app/InputsData.php
@@ -72,10 +72,10 @@ class InputsData extends AbstractApp
                     $this->processingOther();
             }
             if ($this->current[0] == 1 || $this->current[1] == 1) {
-                $total = 1;
+                $total ??= 1;
                 $this->current[2] = 1;
             } elseif ((string)$this->current[0] === '0' || (string)$this->current[1] === '0') {
-                $total = $total ?: 0;
+                $total = 0;
                 $this->current[2] = 0;
             } else {
                 $this->current[2] = $this->current[0] ?: $this->current[1];
